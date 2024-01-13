b20f6b35d655ff706027cf70d8e6d1a07cd7cb5e
Ryan Long <ryan.long@noaa.gov>
2024-01-13 17:44:55 -0500

    Add doco from chatGPT

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
+# Importing necessary modules
 import os
 import time
 from typing import List
 from zipfile import ZipFile
 from pathlib import Path
-import os
 import shutil
 from loguru import logger
 from tqdm import tqdm
 import click
 
-# How many minutes in 30 days
-
+# Constants for time calculation
 HOUR = 3600
 DAY = HOUR * 24
 
+# Root directory of the script
 ROOT = os.path.dirname(os.path.realpath(__file__))
 
+# Default download directory
 DOWNLOAD_DIRECTORY = Path(os.path.join(Path.home(), "Downloads"))
 
+# Logging setup
 logger.remove()
 logger.add(
     os.path.join(DOWNLOAD_DIRECTORY, "LOG"),
@@ -28,7 +30,7 @@ logger.add(
 
 
 def remove(_path: Path) -> None:
-    """deletes a file or path from the filesystem"""
+    """Deletes a file or path from the filesystem."""
     if _path.is_file():
         Path.unlink(_path)
     else:
@@ -36,24 +38,24 @@ def remove(_path: Path) -> None:
 
 
 def get_folder_items(_path: Path) -> List[Path]:
-    """returns a list of files and folder from a directory"""
+    """Returns a list of files and folders from a directory."""
     root, folders, files = next(os.walk(_path))
     return [Path(os.path.join(Path(root, (x)))) for x in folders + files]
 
 
 def is_archivable(_path: Path, age: int) -> bool:
-    """return true if _path is archivable"""
+    """Returns True if _path is archivable."""
     test = time.time() - (age * DAY)
     actual = os.path.getmtime(_path)
     return test > actual
 
 
 @click.command()
-@click.option("--age", default=30, help="archive age in days")
+@click.option("--age", default=30, help="Archive age in days.")
 @click.option("--dryrun/--no-dryrun", default=False)
 def archive(age, dryrun):
-    """Archive items in Download folder older than [AGE].  Defaults to 30 days."""
-    logger.info("starting")
+    """Archive items in the Download folder older than [AGE]. Defaults to 30 days."""
+    logger.info("Starting archive process.")
     archivable = [
         item
         for item in get_folder_items(DOWNLOAD_DIRECTORY)
@@ -63,10 +65,10 @@ def archive(age, dryrun):
     archive_path = os.path.join(DOWNLOAD_DIRECTORY, "archive.zip")
     with ZipFile(archive_path, "a") as myzip:
         for _file in tqdm(archivable):
-            logger.info(f"writing {_file} to zip")
+            logger.info(f"Writing {_file} to zip.")
             if not dryrun:
                 myzip.write(_file)
-            logger.info(f"deleting {_file} to zip")
+            logger.info(f"Deleting {_file}.")
             if not dryrun:
                 remove(_file)
 
